0081f8d2e633cc344cea1665e7eb2adac9a188ed
postite <blog@postite.com>
2019-05-10 17:02:26 +0200

    add uuid()

--- src/Mots.hx
+++ src/Mots.hx
@@ -95,5 +95,18 @@ class Mots {
 		s = r1.map(s, reg -> " " + reg.matched(0).toLowerCase());
 		return s;
 	}
+
+	public static function uuid() {
+		// Based on https://gist.github.com/LeverOne/1308368
+		var uid = new StringBuf(), a = 8;
+		uid.add(StringTools.hex(Std.int(Date.now().getTime()), 8));
+		while((a++) < 36) {
+			uid.add(a*51 & 52 != 0
+				? StringTools.hex(a^15 != 0 ? 8^Std.int(Math.random() * (a^20 != 0 ? 16 : 4)) : 4)
+				: "-"
+			);
+		}
+		return uid.toString().toLowerCase();
+	}
  
 }
